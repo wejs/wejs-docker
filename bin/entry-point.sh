9a05a2c043d318187bc090ee8df077e07d6f2000
albertosouza <alberto.souza.99@gmail.com>
2020-04-03 00:18:53 -0300

    Improve entry-point npm install command

--- bin/entry-point.sh
+++ bin/entry-point.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 DOT=$PWD
-
 NPMDIR="${DOT}/node_modules"
-
 PKG="${DOT}/package.json"
 
 if [ ! -f "$PKG" ]; then
-  echo 'Package.json not found. Are you running a We.js project?'
+  echo 'Package.json not found. Are you running a We.js project or in project folder?'
   exit 1
 fi
 
 if [ ! -d "$NPMDIR" ]; then
-  echo 'NPM module does not exists, try to install with npm install --production'
-  npm install --production
+  echo "NPM module does not exists, running 'npm install --no-audit' with NODE_ENV=${NODE_ENV}"
+  npm install --no-audit
 fi
 
 exec "$@"
